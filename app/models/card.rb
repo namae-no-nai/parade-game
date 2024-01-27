f5f7cb3f4fb02415117211e7cf5ef921851f485b
PedroHamzehT <pedrohamzeh@gmail.com>
2024-01-26 23:08:09 -0300

    I do not know the best commit here, that is a lot of code

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,9 @@
 class Card < ApplicationRecord
-	validates :suit, :value, presence: true
-	has_one :player_card
-	has_one :owner, through: :player_card
+  validates :suit, :value, presence: true
+  has_one :player_card
+  has_one :owner, through: :player_card
 
-	def self.shuffled_deck
-		all.shuffle
-	end
+  def self.shuffled_deck
+    all.shuffle
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'games#index'
+
   post '/initialize_game', to: 'games#initialize_game'
+  get '/game/:id', to: 'games#game', as: 'game'
 end
